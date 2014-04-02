bafa45218a2275d625b1ebf1041f0307fdab97ff
Vianney le Clément de Saint-Marcq <vianney.le.clement@be.pwc.com>
2014-04-02 09:36:30 +0200

    Add configure script

---
+++ configure.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+# Configuration script
+# Copyright 2014 European Union
+#
+# Licensed under the EUPL, Version 1.1 or - as soon they
+# will be approved by the European Commission - subsequent
+# versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the
+# Licence.
+# You may obtain a copy of the Licence at:
+# http://ec.europa.eu/idabc/eupl
+#
+# Unless required by applicable law or agreed to in
+# writing, software distributed under the Licence is
+# distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied.
+# See the Licence for the specific language governing
+# permissions and limitations under the Licence.
+
+set -e -u
+
+# Default values
+BASEURI="http://data.ydmed.gov.gr/"
+ROOTURI="file://gr-pilot/"
+GOOGLE_ANALYTICS="UA-38243808-1"
+
+# Usage notice
+usage() {
+    cat <<EOF
+Usage: $0 [options]
+
+Options:
+  -b URI    The base URI (default: ${BASEURI})
+  -r URI    The (internal) URI of the root directory
+              (default: ${ROOTURI})
+  -a KEY    Google Analytics key (default: ${GOOGLE_ANALYTICS})
+  -h        This help screen
+EOF
+}
+
+# Parse arguments
+while getopts 'b:r:h' opt; do
+    case "${opt}" in
+    b) BASEURI=${OPTARG%/}/ ;;
+    r) ROOTURI=${OPTARG%/}/ ;;
+    a) GOOGLE_ANALYTICS=${OPTARG} ;;
+    h) usage ; exit 0 ;;
+    *) usage ; exit 1 ;;
+    esac
+done
+
+# Construct sed replacement script
+sed=""
+for var in BASEURI ROOTURI GOOGLE_ANALYTICS; do
+    eval "value=\$${var}"
+    sed="${sed}s|%${var}%|${value}|g;"
+done
+
+# Generate files
+find -name '*.in' |
+while read -r infile; do
+    outfile=${infile%.in}
+    printf 'Writing %s.\n' "${outfile}"
+    cat "${infile}" | sed "${sed}" > "${outfile}"
+done
